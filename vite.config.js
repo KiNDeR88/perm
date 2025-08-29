c7f31544a1d3ecf36711be5b4a8b387d097071f9
Алексей Меляков <Polzovatel@MacBook-Pro-Ben.local>
2025-08-29 12:11:46 +0300

    Обновление проекта: правки vite.config.js и package.json

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/perm/',
   plugins: [react()],
-  server: {
-    proxy: {
-      '/pb': {
-        target: 'https://site-v2.apipb.ru',
-        changeOrigin: true,
-        secure: false,
-        rewrite: p => p.replace(/^\/pb/, ''),
-      },
-    },
-    hmr: { overlay: true },
-  },
 })
